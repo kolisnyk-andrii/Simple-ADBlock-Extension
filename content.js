f95de147d520977f8600bf36210e33be0162e170
Andrii Kolisnyk <andrii.kolisnyk.work@gmail.com>
2025-02-28 10:27:08 +0100

    Add files via upload

---
+++ content.js
@@ -0,0 +1,32 @@
+"use strict"
+
+const adList =
+[
+    "*://*.doubleclick.net/*",
+    "*://*.googlesyndication.com/*",
+    "*://*.googletagservices.com/*",
+    "*://*.googleadservices.com/*",
+    "*://*.google-analytics.com/*",
+    "*://*.googleadservices.com/*",
+    "*://*.zedo.com/*",
+    "*://*.adbrite.com/*",
+    "*://*.adbureau.net/*",
+    "*://*.carbonads.net/*",
+    "*://*.cdn.carbonads.com/*",
+    "*://*.cdn.carbonads.net/*",
+    "*://*.cdn.doubleclick.net/*",
+    "*://*.cdn.googletagservices.com/*",
+    "*://*.cdn.googleadservices.com/*",
+    "*://*.cdn.zedo.com/*",
+    "*://*.sarafan.fun/*"
+];
+
+chrome.webRequest.onBeforeRequest.addListener
+(
+    function(details)
+    {
+        return {cancel: true}
+    },
+    {urls: adList},
+    ["blocking"]
+)
